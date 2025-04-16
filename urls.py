ad4bdcadc742da48a845d720c6dd3a76ae404c0f
Janagama Sri Sai Arun <janagamssarun22@gmail.com>
2025-04-16 09:40:00 +0530

    Removed llp app and added new root urls.py

--- urls.py
+++ urls.py
@@ -3,8 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', include('calc.urls')),  # Ensure calc is included
+    path('', include('calc.urls')),  # Routes all app URLs from `calc`
 ]
-
-    
